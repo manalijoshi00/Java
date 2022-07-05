431ce970e510b9ae3b449b475391ab8598a0eef1
manalijoshi00 <108450658+manalijoshi00@users.noreply.github.com>
2022-07-05 11:53:14 +0530

    Add files via upload

---
+++ AsciiValue.java
@@ -0,0 +1,11 @@
+class AsciiValue
+{
+  public static void main(String[] args)
+  {
+    char letter ='A';
+    int ascii = letter;
+    char alp = (char)ascii;
+    System.out.println("The int value of letter" + ascii);
+    System.out.println("The char value of letter" + alp);
+  }
+}
