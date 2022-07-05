f4a44732f4e17e3338a80d7a581a4d925caf950c
manalijoshi00 <108450658+manalijoshi00@users.noreply.github.com>
2022-07-05 09:22:27 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,13 @@
+class Swapping 
+{
+  public static void main(String[] args)
+  {
+    int x = 100;
+    int y = 200;
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+    System.out.println("x =" + x + " and y = " + y);
+  }
+}
