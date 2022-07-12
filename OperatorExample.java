992a472f174d7971fb219e9420f1885f810f66fa
manalijoshi00 <108450658+manalijoshi00@users.noreply.github.com>
2022-07-12 12:06:40 +0530

    Add files via upload

---
+++ OperatorExample.java
@@ -0,0 +1,13 @@
+class OperatorExample
+{
+     public static void main(String args[])
+     {
+        int a=18;
+        int b=7;
+        System.out.println(a+b); 
+        System.out.println(a-b); 
+        System.out.println(a*b); 
+        System.out.println(a/b); 
+        System.out.println(a%b); 
+     }
+}
